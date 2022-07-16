d5c3307eb0afcc8ffcf52077784f1217a0d61df3
Literally 1984 <82011817+Literally-1984@users.noreply.github.com>
2022-07-15 21:03:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -614,8 +614,7 @@ def get_requested_auctions_w(messages, type_soul, num_souls):
 async def wtb(ctx: interactions.CommandContext):
 
     global messages
-
-    messages = ctx.channel.get_history(limit=100)
+    messages = await ctx.channel.get_history(limit=100)
 
     embed = interactions.Embed(title="Soul Sell Offer Finder", description="What kind of souls do you want?",
                                color=0x911ef5)
