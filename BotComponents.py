89e9b591952256b568330804a6872bb23ce82c2c
Literally 1984 <82011817+Literally-1984@users.noreply.github.com>
2022-08-03 16:42:50 -0700

    Update BotComponents.py

--- BotComponents.py
+++ BotComponents.py
@@ -21,6 +21,12 @@ price_htol = interactions.SelectOption(
     description="Sort By Price - Highest to Lowest",
 )
 
+price_custom = interactions.SelectOption(
+    label="Price (Custom Range)",
+    value="price_h",
+    description="Sort By Price - Highest to Lowest",
+)
+
 admin_souls = interactions.SelectOption(
     label="Admin Souls",
     value="admin",
@@ -39,37 +45,49 @@ other_souls = interactions.SelectOption(
     description="Containing Non-Admin and Non-Hypixel Souls Only",
 )
 
-sort_menu = interactions.SelectMenu(
-    options=[price_ltoh, price_htol, admin_souls, hypixel_souls, other_souls],
+sort_price_menu = interactions.SelectMenu(
+    options=[price_ltoh, price_htol, price_custom],
+    placeholder="Sort by.....",
+    custom_id="sort_menu",
+)
+
+sort_admins_menu = interactions.SelectMenu(
+    options=[admin_souls, hypixel_souls, other_souls],
     placeholder="Sort by.....",
     custom_id="sort_menu",
 )
 
 next_page = interactions.Button(
     style=interactions.ButtonStyle.PRIMARY,
-    label="<:arrow_forward:>",
+    label=":arrow_forward:",
     custom_id="arrow_next"
 )
 
 prev_page = interactions.Button(
     style=interactions.ButtonStyle.PRIMARY,
-    label="<:arrow_backward:>",
+    label=":arrow_backward:",
     custom_id="arrow_prev"
 )
 
-num_souls = interactions.TextInput(
+min_price = interactions.TextInput(
     style=interactions.TextStyleType.SHORT,
-    label="How many souls would you like?",
-    custom_id="num_souls",
-    min_length=1,
-    max_length=1,
+    label="Minimum",
+    custom_id="minimum",
+)
+
+max_price = interactions.TextInput(
+    style=interactions.TextStyleType.SHORT,
+    label="Maximum",
+    custom_id="maximum",
+)
+
+price_modal = interactions.Modal(
+        title="Souls Form",
+        custom_id="souls_form",
+        components=[min_price, max_price],
 )
 
 pages_row = interactions.ActionRow.new(next_page, prev_page)
-menu_row = interactions.ActionRow.new(sort_menu)
+price_menu_row = interactions.ActionRow.new(sort_price_menu)
+souls_menu_row = interactions.ActionRow.new(sort_admins_menu)
 
-souls_modal = interactions.Modal(
-        title="Souls Form",
-        custom_id="souls_form",
-        components=[num_souls],
-)
