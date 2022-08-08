6d5cdf2f86ef0ef9931b12d846586fec7e3f15cc
Literally 1984 <82011817+Literally-1984@users.noreply.github.com>
2022-08-07 21:09:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -379,11 +379,11 @@ async def necroauctions(ctx: interactions.CommandContext, user: str):
     await ctx.send(embeds=[embed], components=[BotComponents.price_menu_row, BotComponents.souls_menu_row, BotComponents.pages_row])
 
 @bot.event(name="on_message_create")
-async def on_message_create(ctx: interactions.CommandContext, message: interactions.Message):
+async def on_message_create(ctx: interactions.Message):
   
     if "hp" in message.content and "dmg" in message.content:
         
-        await ctx.send("test")
+        print("sdfsdf")
     
 
 @bot.component("sort_price_menu")
