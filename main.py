2fdd5ba19000c934d0ca9ca08ae9420f771308f2
Literally 1984 <82011817+Literally-1984@users.noreply.github.com>
2022-08-08 08:16:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -578,6 +578,55 @@ async def menu_response(ctx: interactions.CommandContext, options: list[str]):
 
             await ctx.send(embeds=[embed])
 
+@bot.event(name="on_message_create")
+async def on_message_create(message: interactions.Message):
+  
+    if "hp" in message.content and "dmg" in message.content:
+      
+        hp = ""
+        dmg = ""
+        
+        thingbool = False
+        
+        for char in message.content:
+            
+            hp.
+          
+            if(thingbool == True):
+              
+            else:
+              
+              if char == " "
+            
+      
+        if (hp.endswith('m')):
+
+          hp_int = int(hp[:-1]) * 1000000
+
+        elif (hp.endswith('k')):
+
+          hp_int = int(hp[:-1]) * 1000
+
+        if (damage.endswith('m')):
+
+          damage_int = int(damage[:-1]) * 1000000
+
+        elif (damage.endswith('k')):
+
+          damage_int = int(damage[:-1]) * 1000
+
+        manacost = damage_int / 50 + hp_int / 100000
+
+        manacostmin = manacost * 0.268 * (100 - breeze) * 0.01
+
+        embed = interactions.Embed(title="**Mana Cost Calculation**",
+                                  description="**Summon Stats**:\n\n　**HP**: {health}\n\n　**Damage**: {dmg}\n\n\n**Mana Cost**:\n\n　**Raw Mana Cost**: {mana}\n\n　**With Max Reduction (Wise Dragon, breeze on equipment)**: {manamin}"
+                                  .format(health=hp, dmg=damage, mana=manacost, manamin=manacostmin), color=0x911ef5)
+
+        await message.channel.send(embeds=[embed])
+  
+    await message.channel.send()
+
 @bot.component("arrow_prev")
 async def button_response(ctx: interactions.CommandContext):
     await ctx.edit("balls")
